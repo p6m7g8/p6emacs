4ff4a832bed6058991945bd8c3ee378e52408b3a
Philip M. Gollucci <pgollucci@p6m7g8.com>
2016-11-09 20:53:28 -0500

    move to verticle

--- personal/ido.el
+++ personal/ido.el
@@ -1,13 +1,16 @@
 (package 'flx-ido)
 (package 'flx-isearch)
+(package 'ido-vertical-mode)
 
 (ido-mode t)
 (setq ido-enable-flex-matching t) ; fuzzy matching is a must have
 (setq ido-enable-last-directory-history nil) ; forget latest selected directory names
 
-(require 'flx-ido)
-(ido-mode 1)
 (ido-everywhere 1)
 (flx-ido-mode 1)
+
 ;; disable ido faces to see flx highlights.
 (setq ido-use-faces nil)
+
+(ido-vertical-mode 1)
+(setq ido-vertical-define-keys 'C-n-and-C-p-only)
