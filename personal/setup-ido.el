24355513af3334152e35482d463129ecd54a072c
Philip M. Gollucci <pgollucci@p6m7g8.com>
2016-11-13 19:26:34 -0500

    sort, + ubiquitious

--- personal/setup-ido.el
+++ personal/setup-ido.el
@@ -1,18 +1,18 @@
 (package 'flx-ido)
 (package 'flx-isearch)
 (package 'ido-vertical-mode)
+(package 'ido-ubiquitous)
 
-(ido-mode t)
+(ido-mode 1)
+(ido-everywhere 1)
+(flx-ido-mode 1)
+(ido-vertical-mode 1)
+(ido-ubiquitous-mode 1)
+
+(setq ido-use-faces nil) ; disable ido faces to see flx highlights.
 (setq ido-enable-flex-matching t) ; fuzzy matching is a must have
 (setq ido-enable-last-directory-history nil) ; forget latest selected directory names
 
-(ido-everywhere 1)
-(flx-ido-mode 1)
-
-;; disable ido faces to see flx highlights.
-(setq ido-use-faces nil)
-
-(ido-vertical-mode 1)
 (setq ido-vertical-define-keys 'C-n-and-C-p-only)
 
 (provide 'setup-ido)
