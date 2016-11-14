6850ad2d2cebe7258108714750ee13855e8b1f76
Philip M. Gollucci <pgollucci@p6m7g8.com>
2016-11-13 20:15:20 -0500

    more utf8

--- personal/setup-utf8.el
+++ personal/setup-utf8.el
@@ -1,8 +1,14 @@
 (require 'un-define "un-define" t)
+
 (set-buffer-file-coding-system 'utf-8 'utf-8-unix)
 (set-default buffer-file-coding-system 'utf-8-unix)
 (set-default-coding-systems 'utf-8-unix)
-(prefer-coding-system 'utf-8-unix)
 (set-default default-buffer-file-coding-system 'utf-8-unix)
+
+(setq locale-coding-system 'utf-8) ; pretty
+(set-terminal-coding-system 'utf-8) ; pretty
+(set-keyboard-coding-system 'utf-8) ; pretty
+(set-selection-coding-system 'utf-8) ; please
+(prefer-coding-system 'utf-8) ; with sugar on top
 
 (provide 'setup-utf8)
