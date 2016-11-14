a2d246be83efc76c3b9ade100711f4f2149917f9
Philip M. Gollucci <pgollucci@p6m7g8.com>
2016-11-13 20:25:21 -0500

    split hs out

--- personal/setup-global.el
+++ personal/setup-global.el
@@ -65,13 +65,6 @@
 (setq ispell-list-command "list")
 (setq ispell-extra-args '("--sug-mode=ultra"))
 
-;; Show-hide
-(global-set-key (kbd "") 'hs-show-block)
-(global-set-key (kbd "") 'hs-show-all)
-(global-set-key (kbd "") 'hs-hide-block)
-(global-set-key (kbd "") 'hs-hide-all)
-(add-hook 'prog-mode-hook #'hs-minor-mode)
-
 ;; Seed the random-number generator
 (random t)
 
---
+++ personal/setup-hs.el
@@ -0,0 +1,7 @@
+;; Show-hide
+(global-set-key (kbd "") 'hs-show-block)
+(global-set-key (kbd "") 'hs-show-all)
+(global-set-key (kbd "") 'hs-hide-block)
+(global-set-key (kbd "") 'hs-hide-all)
+
+(add-hook 'prog-mode-hook #'hs-minor-mode)
