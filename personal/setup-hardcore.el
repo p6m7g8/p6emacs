1279c1a6d2286c5b35a732a4b1c0d83395ce19cf
Philip M. Gollucci <pgollucci@p6m7g8.com>
2016-11-16 19:58:01 -0500

    disable hardcore

--- personal/setup-hardcore.el
+++ personal/setup-hardcore.el
@@ -1,7 +1,9 @@
 (package 'hardcore-mode)
 
-(require 'hardcore-mode)
-(global-hardcore-mode)
+(setq too-hardcore-backspace t)
+(setq too-hardcore-return t)
+;;(require 'hardcore-mode)
+;;(global-hardcore-mode)
 
 (define-key key-translation-map [?\C-h] [?\C-?])
 (global-set-key (kbd "<f1>") 'help-command)
