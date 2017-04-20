14c216a07d86d28d51637616745c2b67faeaf80c
Philip M. Gollucci <pgollucci@p6m7g8.com>
2017-04-19 20:21:08 -0400

    fix (

--- personal/setup-perl.el
+++ personal/setup-perl.el
@@ -1,6 +1,6 @@
 (package 'tt-mode)
 
-add-to-list 'auto-mode-alist '("\\.pl$" . cperl-mode))
-add-to-list 'auto-mode-alist '("\\.pm$" . cperl-mode))
+(add-to-list 'auto-mode-alist '("\\.pl$" . cperl-mode))
+(add-to-list 'auto-mode-alist '("\\.pm$" . cperl-mode))
 
 (provide 'setup-perl)
