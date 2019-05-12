1be22be27e38c4680794bdc2b15a75399b0b37f0
Philip M. Gollucci <pgollucci@p6m7g8.com>
2019-05-11 21:30:51 -0400

    eternal utf8 hell, not in 26.2

--- personal/setup-utf8.el
+++ personal/setup-utf8.el
@@ -3,7 +3,7 @@
 (set-buffer-file-coding-system 'utf-8 'utf-8-unix)
 (set-default buffer-file-coding-system 'utf-8-unix)
 (set-default-coding-systems 'utf-8-unix)
-(set-default default-buffer-file-coding-system 'utf-8-unix)
+;(set-default default-buffer-file-coding-system 'utf-8-unix)
 
 (setq locale-coding-system 'utf-8) ; pretty
 (set-terminal-coding-system 'utf-8) ; pretty
