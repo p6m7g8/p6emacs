ae445086c2c108e304c9013091770a33c675d5e8
Philip M. Gollucci <pgollucci@p6m7g8.com>
2019-08-16 01:32:34 -0400

    docker

---
+++ personal/setup-docker.el
@@ -0,0 +1,9 @@
+(package 'dockerfile-mode)
+(add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode))
+(put 'dockerfile-image-name 'safe-local-variable #'stringp)
+
+(package 'docker-compose-mode)
+
+(package 'docker)
+(setq docker-image-run-arguments '("-i" "-t" "--rm"))
+
