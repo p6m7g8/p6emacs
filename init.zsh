49cfbc8234d67b85ddf2c8540ca906444f68e3d3
Philip M. Gollucci <pgollucci@p6m7g8.com>
2019-09-11 20:43:43 -0400

    more

--- init.zsh
+++ init.zsh
@@ -1,3 +1,14 @@
 p6df::modules::p6emacs::version() { echo "0.0.1" }
-p6df::modules::p6emacs::deps()    { ModuleDeps=(john2x/jenkinsfile-mode) }
+p6df::modules::p6emacs::deps()    {
+    ModuleDeps=(
+	john2x/jenkinsfile-mode
+	Silex/docker.el
+    )
+}
 
+p6df::modules::p6emacs::home::symlink() {
+
+    ln -fs $P6_DFZ_SRC_DIR/Silex/docker.el $P6_DFZ_SRC_P6M7G8_DIR/p6emacs/elpa/docker-20190730.711
+
+
+}
